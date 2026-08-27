9d6d1549682aeecf132e6012c5bba3779ff8c2cb
Samuel Faber <samuel.faber@live.fr>
2026-08-27 12:51:55 +0200

    update: ajout de mlflow dans les requirements et passage en v0.0.3

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("requirements.txt") as f:
 requirements = [x.strip() for x in content if "git+" not in x]
 
 setup(name='mastershelf',
-      version="0.0.2",
+      version="0.0.3",
       description="Master Shelf",
       license="MIT",
       author="Le Wagon",
