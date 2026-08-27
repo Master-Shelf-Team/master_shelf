9ebee6c86a158fac56ba07f4f7faa20b92d4021d
Samuel Faber <samuel.faber@live.fr>
2026-08-27 16:33:13 +0200

    feat: récupération de la vraie image front et traitement

--- mastershelf/api/fast.py
+++ mastershelf/api/fast.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File, Form
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
@@ -7,6 +7,8 @@ import cv2
 from pathlib import Path
 from mastershelf.algo_filter.filtering import final_match
 from mastershelf.vision.detector import yolo_predict
+from PIL import Image
+import io
 
 app = FastAPI()
 
@@ -25,12 +27,19 @@ async def favicon():
 
 # http://127.0.0.1:8000/ingredients POST avec TON JPEG > à corriger quand on sera rendu là
 @app.post("/ingredients")
-def ingredients(
-        image: str
-    ):      # 1
+async def ingredients(
+    file: UploadFile = File(...),
+    # data: str = Form(...)  # Décommentez si vous devez aussi récupérer 'data'
+):
+    # 1. Lire le contenu du fichier envoyé par le front
     """
     Renvoie les ingrédients trouvés dans l'image
     """
+    contents = await file.read()
+
+    # 2. Convertir en image PIL
+    image = Image.open(io.BytesIO(contents))
+
     ing_list = yolo_predict(image)
 
     print(f"🔎 Recherche de la meilleure recette pour la liste d'ingrédients 🔎")
