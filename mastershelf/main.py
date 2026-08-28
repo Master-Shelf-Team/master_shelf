13f4df63ea11ac2bbce82252a10d032d5cf59d8e
Samuel Faber <samuel.faber@live.fr>
2026-08-28 13:33:12 +0200

    update: Changement de final_match par la Query BigQuery

--- mastershelf/main.py
+++ mastershelf/main.py
@@ -1,6 +1,8 @@
 from mastershelf.recipes.preprocessing import load_recipes
 from mastershelf.algo_filter.filtering import *
-result = final_match()
+from mastershelf.inventory.inventory import PHOTO_INV
 
+result = get_matching_recipes(PHOTO_INV)
+print("On affiche les résultats")
 for i in result:
     print(i)
